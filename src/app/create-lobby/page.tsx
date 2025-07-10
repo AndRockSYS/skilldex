d66b03160a3af37a16629d519b32aa072a5fec00
Andris <106549955+AndRockSYS@users.noreply.github.com>
2025-07-10 17:20:44 +0300

    chore: stake conversion

--- src/app/create-lobby/page.tsx
+++ src/app/create-lobby/page.tsx
@@ -78,7 +78,7 @@ export default function CreateLobby() {
         defaultValues: {
             gameType: games.find((g) => g.id == Number(gameTypeId))?.id ?? GameType.TicTacToe,
             token: Token.SOL,
-            stake: stake ? Number(stake) : MIN_STAKE,
+            stake: stake ? Number(stake) / LAMPORTS_PER_SOL : MIN_STAKE,
             expiration: EXPIRATION_OPTIONS[0].id,
             turnTimeLimit: TURN_LIMITS[0].id,
             matchFormat: Object.values(MatchFormat).includes(formatId)
