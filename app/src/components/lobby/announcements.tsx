a67f76c7f2521609b92756ffc499e642a10e80fa
AndRockSYS <106549955+AndRockSYS@users.noreply.github.com>
2025-07-16 10:04:41 +0300

    fix: announcement closure

--- app/src/components/lobby/announcements.tsx
+++ app/src/components/lobby/announcements.tsx
@@ -30,37 +30,42 @@ export default function Announcements() {
         initialData: [],
     });
 
-    return announcements
-        .filter(
-            (ann) =>
-                ann.createdAt >
-                    Number(localStorage.getItem(LOCAL_KEY_ANNOUNCEMENT_TIMESTAMP) ?? 0) || isClosed
-        )
-        .map((announcement) => (
-            <Alert
-                key={announcement.createdAt}
-                className='relative bg-primary/10 border-primary/30'
-            >
-                <Megaphone className='h-5 w-5 text-primary' />
-                <AlertTitle className='font-headline text-primary'>{announcement.title}</AlertTitle>
-                <AlertDescription className='text-primary/80 whitespace-pre-wrap'>
-                    {announcement.content}
-                </AlertDescription>
-                <Button
-                    variant='ghost'
-                    size='icon'
-                    className='absolute top-2 right-2 min-w-4 min-h-4 text-primary/70 hover:text-primary'
-                    onClick={() => {
-                        localStorage.setItem(
-                            LOCAL_KEY_ANNOUNCEMENT_TIMESTAMP,
-                            announcement.createdAt.toString()
-                        );
-                        setIsClosed(true);
-                    }}
-                    title='Dismiss announcement'
+    return (
+        !isClosed &&
+        announcements
+            .filter(
+                (ann) =>
+                    ann.createdAt >
+                    Number(localStorage.getItem(LOCAL_KEY_ANNOUNCEMENT_TIMESTAMP) ?? 0)
+            )
+            .map((announcement) => (
+                <Alert
+                    key={announcement.createdAt}
+                    className='relative bg-primary/10 border-primary/30'
                 >
-                    <X className='absolute top-2 right-2 h-4 w-4' />
-                </Button>
-            </Alert>
-        ));
+                    <Megaphone className='h-5 w-5 text-primary' />
+                    <AlertTitle className='font-headline text-primary'>
+                        {announcement.title}
+                    </AlertTitle>
+                    <AlertDescription className='text-primary/80 whitespace-pre-wrap'>
+                        {announcement.content}
+                    </AlertDescription>
+                    <Button
+                        variant='ghost'
+                        size='icon'
+                        className='absolute top-2 right-2 min-w-4 min-h-4 text-primary/70 hover:text-primary'
+                        onClick={() => {
+                            localStorage.setItem(
+                                LOCAL_KEY_ANNOUNCEMENT_TIMESTAMP,
+                                announcement.createdAt.toString()
+                            );
+                            setIsClosed(true);
+                        }}
+                        title='Dismiss announcement'
+                    >
+                        <X className='absolute top-2 right-2 h-4 w-4' />
+                    </Button>
+                </Alert>
+            ))
+    );
 }
