e18f35974b69babaf34ea24afae3e9323d8d800a
AndRockSYS <106549955+AndRockSYS@users.noreply.github.com>
2025-08-18 19:17:52 +0000

    fix: added X1

--- app/src/utils/formatter.tsx
+++ app/src/utils/formatter.tsx
@@ -1,29 +1,34 @@
-import { Gamepad2 } from 'lucide-react';
+import { Gamepad2 } from "lucide-react";
 
-import { LAMPORTS_PER_SOL, PublicKey } from '@solana/web3.js';
+import { LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
 
-import { games } from '@/content/games';
-import taunts from '@/content/taunts';
+import { games } from "@/content/games";
+import taunts from "@/content/taunts";
 
-import { getTokenName, LobbyDisplayStatus, Token } from '@/types/utils';
-import { GameState, GameType, getGameName, Lobby } from '@/types/games';
-import { Timestamp } from '@/types/utils';
+import { getTokenName, LobbyDisplayStatus, Token } from "@/types/utils";
+import { GameState, GameType, getGameName, Lobby } from "@/types/games";
+import { Timestamp } from "@/types/utils";
 
-export const formatTokenAmount = (amount: number | BigInt, token: Token): number => {
+export const formatTokenAmount = (
+    amount: number | BigInt,
+    token: Token
+): number => {
     switch (token) {
         case Token.SOL:
+            return Number(amount) / LAMPORTS_PER_SOL;
+        case Token.X1:
             return Number(amount) / LAMPORTS_PER_SOL;
     }
 };
 
 export const formatWallet = (wallet: string) => {
-    if (!wallet) return 'N/A';
+    if (!wallet) return "N/A";
     return `${wallet.substring(0, 5)}...${wallet.substring(wallet.length - 5)}`;
 };
 
 export const GameIcon = ({ id }: { id: GameType }) => {
     const gameDefinition = games.find((g) => g.id == id);
-    const className = 'inline h-5 w-5 mr-2 rounded-sm shrink-0';
+    const className = "inline h-5 w-5 mr-2 rounded-sm shrink-0";
 
     return gameDefinition ? (
         <gameDefinition.icon className={className} />
@@ -35,28 +40,28 @@ export const GameIcon = ({ id }: { id: GameType }) => {
 export function getLobbyDisplayStatus(lobby: Lobby): LobbyDisplayStatus {
     if (lobby.state == GameState.Open) {
         return lobby.creator.txSignature
-            ? { text: 'Open', variant: 'default' }
-            : { text: 'Initializing', variant: 'secondary' };
+            ? { text: "Open", variant: "default" }
+            : { text: "Initializing", variant: "secondary" };
     }
 
     if (lobby.state == GameState.Active) {
         return lobby.opponent && !lobby.opponent.txSignature
-            ? { text: 'Opponent Staking', variant: 'secondary' }
-            : { text: 'In Play', variant: 'default' };
+            ? { text: "Opponent Staking", variant: "secondary" }
+            : { text: "In Play", variant: "default" };
     }
 
-    return { text: 'Finished', variant: 'outline' };
+    return { text: "Finished", variant: "outline" };
 }
 
 export function formatExpirationTime(expirationTime?: Timestamp): string {
     return expirationTime
         ? new Date(expirationTime).toLocaleTimeString([], {
-              hour: '2-digit',
-              minute: '2-digit',
-              day: '2-digit',
-              month: 'short',
+              hour: "2-digit",
+              minute: "2-digit",
+              day: "2-digit",
+              month: "short",
           })
-        : 'N/A';
+        : "N/A";
 }
 
 export function isValidSolanaPublicKey(publicKey: string): boolean {
@@ -91,11 +96,11 @@ export function generateTaunt(
     const random = Math.floor(Math.random() * 5);
     const taunt = taunts[random];
     return taunt
-        .replace('{creator}', creatorWallet)
-        .replace('{stakeAmount}', (stakeLamports / LAMPORTS_PER_SOL).toFixed(2))
-        .replace('{token}', getTokenName(token))
-        .replace('{gameName}', getGameName(gameType))
-        .replace('{link}', link);
+        .replace("{creator}", creatorWallet)
+        .replace("{stakeAmount}", (stakeLamports / LAMPORTS_PER_SOL).toFixed(2))
+        .replace("{token}", getTokenName(token))
+        .replace("{gameName}", getGameName(gameType))
+        .replace("{link}", link);
 }
 
 export function convertTurnTimeLeft(timeLeft: number): string {
@@ -105,5 +110,7 @@ export function convertTurnTimeLeft(timeLeft: number): string {
     const mins = Math.floor((seconds % 3600) / 60);
     const secs = seconds % 60;
 
-    return [hrs, mins, secs].map((unit) => String(unit).padStart(2, '0')).join(':');
+    return [hrs, mins, secs]
+        .map((unit) => String(unit).padStart(2, "0"))
+        .join(":");
 }
